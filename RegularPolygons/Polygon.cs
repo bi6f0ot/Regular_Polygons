fd77a0bcc4b850a9c395a6b2dba80b9eb3a96a52
bi6f0ot <ehlmalus@gmail.com>
2019-10-24 12:05:46 -0700

    initial commit

---
+++ RegularPolygons/Polygon.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace RegularPolygons {
+    internal class Polygon {
+        internal PointF Loc;
+        internal long Sides;
+        internal float Radius;
+
+        private float Heading;
+
+        private void SetHeading( Point Cursor ) {
+            float Diff_X, Diff_Y;
+            Diff_X = Cursor.X - Loc.X;
+            Diff_Y = Cursor.Y - Loc.Y;
+
+            Heading = (float)Math.Atan2(Diff_Y, Diff_X);
+        }
+
+        public Polygon( Point Cursor ) {
+            SetHeading(Cursor);
+        }
+
+        internal void Update( Point Cursor ) {
+            SetHeading(Cursor);
+        }
+
+        internal void Display( Graphics g ) {
+            g.DrawEllipse(Pens.White, Loc.X - 1, Loc.Y - 1, 2, 2);
+
+            List<PointF> points = new List<PointF>();
+            long Side = 0;
+            float X, Y;
+            for ( float i = Heading; i < Heading + 2 * Math.PI; i += 360 / Sides * ((float)Math.PI / 180) ) {
+                X = Loc.X + Radius * (float)Math.Cos(i);
+                Y = Loc.Y + Radius * (float)Math.Sin(i); ;
+
+                points.Add(new PointF(X, Y));
+                Side++;
+            };
+            points.Add(points[ 0 ]);
+
+            g.DrawLines(Pens.White, points.ToArray());
+        }
+    }
+}
---
+++ RegularPolygons/ViewingWindow.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace RegularPolygons {
+    public partial class ViewingWindow: Form {
+        public ViewingWindow() { InitializeComponent(); }
+
+        private Timer UpdateTimer;
+
+        private List<Polygon> Shapes;
+        private long NumOfShapes = 0;
+
+        private const long MaxWidthOfPolygons = 150;
+
+        private void SetPolygons() {
+            NumOfShapes = ClientSize.Width / MaxWidthOfPolygons;
+
+            Shapes = new List<Polygon>();
+            float X, Y;
+            PointF Location;
+            long NumOfSides;
+            for ( int i = 0; i < NumOfShapes; i++ ) {
+                X = ClientSize.Width / (NumOfShapes + 1) * (i + 1);
+                Y = ClientSize.Height / 2;
+
+                Location = new PointF(X, Y);
+                NumOfSides = i + 2;
+
+                Polygon newPolygon = new Polygon(PointToClient(MousePosition)) {
+                    Loc = Location,
+                    Sides = NumOfSides,
+                    Radius = MaxWidthOfPolygons * .33f
+                };
+
+                Shapes.Add(newPolygon);
+            };
+        }
+
+        private void ViewingWindow_Load( object sender, EventArgs e ) {
+            SetPolygons();
+
+            UpdateTimer = new Timer { Interval = 10 };
+            UpdateTimer.Tick += ( s, ea ) => { Invalidate(); };
+            UpdateTimer.Start();
+        }
+
+        private void ViewingWindow_Resize( object sender, EventArgs e ) {
+            float X, Y;
+            for ( int i = 0; i < NumOfShapes; i++ ) {
+                X = ClientSize.Width / (NumOfShapes + 1) * (i + 1);
+                Y = ClientSize.Height / 2;
+
+                Shapes[ i ].Loc = new PointF(X, Y);
+            };
+        }
+
+        private void ViewingWindow_ResizeEnd( object sender, EventArgs e ) {
+            SetPolygons();
+        }
+
+        private void ViewingWindow_Paint( object sender, PaintEventArgs e ) {
+            foreach ( Polygon P in Shapes ) {
+                P.Update(PointToClient(MousePosition));
+                P.Display(e.Graphics);
+            };
+        }
+    }
+}
